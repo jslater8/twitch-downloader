c71f01e00e5b98a31d8a3bbf31263a41b8d08a01
jslater8 <josh.miniac@gmail.com>
2017-09-20 06:53:37 +0100

    Using call instead of subprocess

--- twitch.py
+++ twitch.py
@@ -7,11 +7,12 @@ import signal
 import os
 import threading
 import psutil
+import streamlink as sl
 
 
 def signal_handler(signal, frame):
     print 'You pressed Ctrl-C'
-    
+
 
 def getName():
     if '-f' in sys.argv:
@@ -22,26 +23,20 @@ def getName():
         sys.exit()
     return name
 
+def isDownload():
+    return '-nd' not in sys.argv
+
 def getStream(name):
-    url = 'https://api.twitch.tv/kraken/streams/' + name
-    contents = urllib2.urlopen(url)
-    return json.loads(contents.read().decode('utf-8'))
+    streams = sl.streams("twitch.tv/" + name)
+    return bool(streams)
 
 def isOnline(name):
-    try:
-        data = getStream(name)
-        if data['stream'] == None:
-            status = 1
-        else:
-            status = 0
-    except:
-        status = 2
-    return status
+    return getStream(name)
 
 
 PROCNAME = "python.exe"
 
-if __name__ == '__main__':
+def oldmain():
     signal.signal(signal.SIGINT, signal_handler)
     running = False
     counter = 0
@@ -50,36 +45,65 @@ if __name__ == '__main__':
     if not os.path.exists(name):
         os.makedirs(name)
     while(True):
-    	online = isOnline(name)
-        if not online:
-            os.chdir(name)
-            start = time.strftime("%d-%m-%H-%M")
-            size = sum(os.path.getsize(f) for f in os.listdir('.') if os.path.isfile(f))
-            if size > 50000000000:
-                print 'size too big'
-                for proc in psutil.process_iter():
-                    if proc.name() == PROCNAME and proc.pid != os.getpid():
-                        proc.kill()
-                break
-            if not running:
-                process = subprocess.Popen(['livestreamer', \
-                 'twitch.tv/' + name, 'best', '-o', \
-                   str(counter) + '--' + str(start) + '.mp4'])
-                counter = counter + 1
-            if process.returncode == None:
-                running = True
+        try:
+            online = isOnline(name)
+            if online:
+                os.chdir(name)
+                start = time.strftime("%d-%m-%H-%M")
+                size = sum(os.path.getsize(f) for f in os.listdir('.') if os.path.isfile(f))
+                if size > 50000000000:
+                    print 'size too big'
+                    for proc in psutil.process_iter():
+                        if proc.name() == PROCNAME and proc.pid != os.getpid():
+                            proc.kill()
+                    break
+                if not running:
+                    print "starting dl"
+                    process = subprocess.Popen(['streamlink', \
+                     'twitch.tv/' + name, 'best', '-o', \
+                       str(counter) + '--' + str(start) + '.mp4'])
+                    counter = counter + 1
+                if process.returncode == None:
+                    running = True
+                else:
+                    running = False
+                os.chdir('..')
             else:
-                running = False
-            os.chdir('..')
-        elif online == 1:
-            print 'not online'
+                print 'not online'
+            time.sleep(10)
+            #print time.time() - time2
+            time2 = time.time()
+        except KeyboardInterrupt:
+            print "keyboard interrupt exiting"
+            exit(0)
         else:
-            print 'not strimmer'
-            sys.exit()
-        time.sleep(5*10)
-        #print time.time() - time2
-        time2 = time.time()
-            
-    
+            print "exception trying again"
 
-    
+if __name__ == '__main__':
+    name = getName()
+    dl = isDownload()
+    if not os.path.exists(name):
+        os.makedirs(name)
+    while(True):
+        try:
+            online = isOnline(name)
+            if online:
+                os.chdir(name)
+                start = time.strftime("%d-%m-%H-%M")
+                size = sum(os.path.getsize(f) for f in os.listdir('.') if os.path.isfile(f))
+                if size > 75*1000*1000*1000:
+                    print "size too big"
+                    exit(1);
+                if dl:
+                    subprocess.call(['streamlink', 'twitch.tv/' + name, 'best', '-o ' + str(start) + '.mp4'])
+                else:
+                    subprocess.call(['streamlink', 'twitch.tv/' + name, 'best'])
+                os.chdir('..')
+            else:
+                print 'offline'
+            time.sleep(10)
+        except KeyboardInterrupt:
+            print 'keyboard'
+            exit(0)
+        except Exception as e:
+            print 'exception' + str(e) + ' trying again'
